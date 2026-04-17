9ee85976f785f5598aa2dca4844ad9c5fbe66b2b
karar-khalid <lhosenekarar8@gmail.com>
2026-04-17 14:14:58 -0700

    oop

---
+++ Backend/models/user.py
@@ -0,0 +1,46 @@
+import json
+class User:
+    users_list = []
+    def __init__(self,first_name,last_name,email,password,account_state=True):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.account_state = account_state
+
+    def add_to_list(self):
+        User.users_list.append(self)
+
+    def to_dict(self):
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "password": self.password,
+            "account_state": self.account_state
+        }
+
+    def display_user(self):
+        print(f"first name: {self.first_name} {self.last_name}")
+        print(f"email: {self.email}")
+        print(f"password: {self.password}")
+        print(f"account state: {self.account_state}")
+
+    @classmethod
+    def save_to_json(cls):
+        try:
+            with open('users.json', 'r') as file:
+                data = json.load(file)
+        except:
+            data={'users':[]}
+        if "users"  not in data:
+            data["users"] = []
+        for user in cls.users_list:
+            data.append(user.to_dict())
+        with open('users.json', "w") as file:
+            json.dump(data,file,indent=4)
+
+
+
+
+
