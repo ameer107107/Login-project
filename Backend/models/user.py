80c54402e4b478502f323a883abe34e9b87c9b51
karar-khalid <lhosenekarar8@gmail.com>
2026-04-22 08:13:35 -0700

    oop

--- Backend/models/user.py
+++ Backend/models/user.py
@@ -1,8 +1,10 @@
 import json
+import atexit
 class User:
     users_list = []
-    def __init__(self,username,email,password,account_state=True):
-        self.username = username
+    def __init__(self,first_name,last_name,email,password,account_state=True):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
         self.account_state = account_state
@@ -11,7 +13,7 @@ class User:
         try:
             with open('users.json', 'r') as file:
                 data = json.load(file)
-        except:
+        except FileNotFoundError:
             return False
         if "users"  in data:
             for user in data["users"]:
@@ -25,14 +27,16 @@ class User:
 
     def to_dict(self):
         return {
-            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
             "password": self.password,
             "account_state": self.account_state
         }
 
     def display_user(self):
-        print(f"username: {self.username}")
+        print(f"first_name: {self.first_name}")
+        print(f"last_name: {self.last_name}")
         print(f"email: {self.email}")
         print(f"password: {self.password}")
         print(f"account state: {self.account_state}")
@@ -54,6 +58,8 @@ class User:
         with open('data.json', "w") as file:
             json.dump(data,file,indent=4)
         cls.users_list=[]
+
+atexit.register(User.save_to_json)
 
 
 
